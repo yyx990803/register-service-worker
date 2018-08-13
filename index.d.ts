2be982734f85f2fad84e5fb0552f5e906ee60e4c
Evan You <yyx990803@gmail.com>
2018-08-13 18:07:55 -0400

    fix(types): add type for updatefound event

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ export type Hooks = {
   registered?: (registration: any) => void;
   cached?: (registration: any) => void;
   updated?: (registration: any) => void;
+  updatefound?: (registration: any) => void;
   offline?: () => void;
   error?: (error: Error) => void;
 };
