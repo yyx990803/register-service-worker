99b3d47223b90e2902a7e0bf373b76b9253f9f51
User1 <arianandcode@gmail.com>
2019-04-24 11:28:00 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -5,39 +5,31 @@
 // will only see deployed updates on the "N+1" visit to a page, since previously
 // cached resources are updated in the background.
 var hostname = window.location.hostname;
-var match = /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/;
-var isLocalhost = function () {
-    return Boolean(
-        hostname === 'localhost' ||
-        // [::1] is the IPv6 localhost address.
-        hostname === '[::1]' ||
-        // 127.0.0.1/8 is considered localhost for IPv4.
-        hostname.match(match)
-    );
-}
+const match = /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/;
+const isLocalhost = () => Boolean(
+    hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    hostname.match(match)
+)
 
-export function register(swUrl, hooks) {
-    if (hooks === void 0) hooks = {};
-
-    var registrationOptions = hooks.registrationOptions;
-    if (registrationOptions === void 0) registrationOptions = {};
+export function register (swUrl, hooks = {}) {
+    const { registrationOptions = {}} = hooks
     delete hooks.registrationOptions
 
-    var emit = function (hook) {
-        var args = [], len = arguments.length - 1;
-        while (len-- > 0) args[len] = arguments[len + 1];
-
+    const emit = (hook, ...args) => {
         if (hooks && hooks[hook]) {
-            hooks[hook].apply(hooks, args)
+            hooks[hook](...args)
         }
     }
 
     if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function () {
+        window.addEventListener('load', () => {
             if (isLocalhost()) {
                 // This is running on localhost. Lets check if a service worker still exists or not.
                 checkValidServiceWorker(swUrl, emit, registrationOptions)
-                navigator.serviceWorker.ready.then(function (registration) {
+                navigator.serviceWorker.ready.then(registration => {
                     emit('ready', registration)
                 })
             } else {
@@ -48,19 +40,19 @@ export function register(swUrl, hooks) {
     }
 }
 
-function registerValidSW(swUrl, emit, registrationOptions) {
+function registerValidSW (swUrl, emit, registrationOptions) {
     navigator.serviceWorker
         .register(swUrl, registrationOptions)
-        .then(function (registration) {
+        .then(registration => {
             emit('registered', registration)
             if (registration.waiting) {
                 emit('updated', registration)
                 return
             }
-            registration.onupdatefound = function () {
+            registration.onupdatefound = () => {
                 emit('updatefound', registration)
-                var installingWorker = registration.installing
-                installingWorker.onstatechange = function () {
+                const installingWorker = registration.installing
+                installingWorker.onstatechange = () => {
                     if (installingWorker.state === 'installed') {
                         if (navigator.serviceWorker.controller) {
                             // At this point, the old content will have been purged and
@@ -78,31 +70,31 @@ function registerValidSW(swUrl, emit, registrationOptions) {
                 }
             }
         })
-        .catch(function (error) {
+        .catch(error => {
             emit('error', error)
         })
 }
 
-function checkValidServiceWorker(swUrl, emit, registrationOptions) {
+function checkValidServiceWorker (swUrl, emit, registrationOptions) {
     // Check if the service worker can be found.
     fetch(swUrl)
-        .then(function (response) {
+        .then(response => {
             // Ensure service worker exists, and that we really are getting a JS file.
             if (response.status === 404) {
                 // No service worker found.
-                emit('error', new Error(("Service worker not found at " + swUrl)))
+                emit('error', new Error(`Service worker not found at ${swUrl}`))
                 unregister()
             } else if (response.headers.get('content-type').indexOf('javascript') === -1) {
                 emit('error', new Error(
-                    "Expected " + swUrl + " to have javascript content-type, " +
-                    "but received " + (response.headers.get('content-type'))))
+                    `Expected ${swUrl} to have javascript content-type, ` +
+                    `but received ${response.headers.get('content-type')}`))
                 unregister()
             } else {
                 // Service worker found. Proceed as normal.
                 registerValidSW(swUrl, emit, registrationOptions)
             }
         })
-        .catch(function (error) {
+        .catch(error => {
             if (!navigator.onLine) {
                 emit('offline')
             } else {
@@ -111,10 +103,11 @@ function checkValidServiceWorker(swUrl, emit, registrationOptions) {
         })
 }
 
-export function unregister() {
+export function unregister () {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.ready.then(function (registration) {
+        navigator.serviceWorker.ready.then(registration => {
             registration.unregister()
         })
     }
 }
+
